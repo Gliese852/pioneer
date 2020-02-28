a10ae0cbc0860a072d237897fb55a55b0229aea2
Gliese852 <erg343@yandex.ru>
2020-02-28 22:59:37 +0300

    use ui.rescaleUI instead of manually scaling, fix identation

--- data/pigui/modules/system-view-ui.lua
+++ data/pigui/modules/system-view-ui.lua
@@ -12,7 +12,7 @@ local player = nil
 local colors = ui.theme.colors
 local icons = ui.theme.icons
 
-local mainButtonSize = Vector2(32,32)  * (ui.screenHeight / 1200)
+local mainButtonSize = ui.rescaleUI(Vector2(32,32), Vector2(1600, 900))
 local mainButtonFramePadding = 3
 local itemSpacing = Vector2(8, 4) -- couldn't get default from ui
 local indicatorSize = Vector2(30 , 30)
@@ -130,79 +130,79 @@ local orbitPlannerWindowPos = Vector2(ui.screenWidth - calcWindowWidth(7), ui.sc
 local function showOrbitPlannerWindow()
 	ui.setNextWindowPos(orbitPlannerWindowPos, "Always")
 	ui.withStyleColors({["WindowBg"] = svColor.WINDOW_BACK}, function()
-			ui.window("OrbitPlannerWindow", {"NoTitleBar", "NoResize", "NoFocusOnAppearing", "NoBringToFrontOnFocus", "NoSavedSettings", "AlwaysAutoResize"},
-								function()
-									ui.text("Orbit planner")
+		ui.window("OrbitPlannerWindow", {"NoTitleBar", "NoResize", "NoFocusOnAppearing", "NoBringToFrontOnFocus", "NoSavedSettings", "AlwaysAutoResize"},
+		function()
+			ui.text("Orbit planner")
 
-									ui.separator()
+			ui.separator()
 
-									if ui.coloredSelectedIconButton(icons.systemmap_reset_view, mainButtonSize, showShips, mainButtonFramePadding, svColor.BUTTON_BACK, svColor.BUTTON_INK, "Reset view") then
-										Engine.SystemMapSetVisibility("RESET_VIEW")
-									end
-									ui.sameLine()
-									if ui.coloredSelectedIconButton(icons.systemmap_toggle_grid, mainButtonSize, showShips, mainButtonFramePadding, svColor.BUTTON_BACK, svColor.BUTTON_INK, "Show grid") then
-										show_grid = nextShowGrid[show_grid]
-										Engine.SystemMapSetVisibility(show_grid);
-									end
-									ui.sameLine()
-									if ui.coloredSelectedIconButton(icons.systemmap_toggle_ships, mainButtonSize, showShips, mainButtonFramePadding, svColor.BUTTON_BACK, svColor.BUTTON_INK, "Show ships") then
-										ship_drawing = nextShipDrawings[ship_drawing]
-										Engine.SystemMapSetVisibility(ship_drawing);
-									end
-									ui.sameLine()
-									if ui.coloredSelectedIconButton(icons.systemmap_toggle_lagrange, mainButtonSize, showLagrangePoints, mainButtonFramePadding, svColor.BUTTON_BACK, svColor.BUTTON_INK, "Show Lagrange points") then
-										show_lagrange = nextShowLagrange[show_lagrange]
-										Engine.SystemMapSetVisibility(show_lagrange);
-									end
-									ui.sameLine()
-									ui.coloredSelectedIconButton(icons.zoom_in,mainButtonSize, false, mainButtonFramePadding, svColor.BUTTON_BACK, svColor.BUTTON_INK, "Zoom in")
-									if ui.isItemActive() then
-										Engine.SystemMapSetVisibility("ZOOM_IN");
-									end
-									ui.sameLine()
-									ui.coloredSelectedIconButton(icons.zoom_out, mainButtonSize, false, mainButtonFramePadding, svColor.BUTTON_BACK, svColor.BUTTON_INK, "Zoom out")
-									if ui.isItemActive() then
-										Engine.SystemMapSetVisibility("ZOOM_OUT");
-									end
+			if ui.coloredSelectedIconButton(icons.systemmap_reset_view, mainButtonSize, showShips, mainButtonFramePadding, svColor.BUTTON_BACK, svColor.BUTTON_INK, "Reset view") then
+				Engine.SystemMapSetVisibility("RESET_VIEW")
+			end
+			ui.sameLine()
+			if ui.coloredSelectedIconButton(icons.systemmap_toggle_grid, mainButtonSize, showShips, mainButtonFramePadding, svColor.BUTTON_BACK, svColor.BUTTON_INK, "Show grid") then
+				show_grid = nextShowGrid[show_grid]
+				Engine.SystemMapSetVisibility(show_grid);
+			end
+			ui.sameLine()
+			if ui.coloredSelectedIconButton(icons.systemmap_toggle_ships, mainButtonSize, showShips, mainButtonFramePadding, svColor.BUTTON_BACK, svColor.BUTTON_INK, "Show ships") then
+				ship_drawing = nextShipDrawings[ship_drawing]
+				Engine.SystemMapSetVisibility(ship_drawing);
+			end
+			ui.sameLine()
+			if ui.coloredSelectedIconButton(icons.systemmap_toggle_lagrange, mainButtonSize, showLagrangePoints, mainButtonFramePadding, svColor.BUTTON_BACK, svColor.BUTTON_INK, "Show Lagrange points") then
+				show_lagrange = nextShowLagrange[show_lagrange]
+				Engine.SystemMapSetVisibility(show_lagrange);
+			end
+			ui.sameLine()
+			ui.coloredSelectedIconButton(icons.zoom_in,mainButtonSize, false, mainButtonFramePadding, svColor.BUTTON_BACK, svColor.BUTTON_INK, "Zoom in")
+			if ui.isItemActive() then
+				Engine.SystemMapSetVisibility("ZOOM_IN");
+			end
+			ui.sameLine()
+			ui.coloredSelectedIconButton(icons.zoom_out, mainButtonSize, false, mainButtonFramePadding, svColor.BUTTON_BACK, svColor.BUTTON_INK, "Zoom out")
+			if ui.isItemActive() then
+				Engine.SystemMapSetVisibility("ZOOM_OUT");
+			end
 
-									ui.separator()
+			ui.separator()
 
-									showDvLine(icons.orbit_reduce, icons.orbit_delta, icons.orbit_increase, "factor", function(i) return i, "x" end, "Decrease delta factor", "Reset delta factor", "Increase delta factor")
-									showDvLine(icons.orbit_reduce, icons.orbit_start_time, icons.orbit_increase, "starttime",
-														 function(i)
-															 local now = Game.time
-															 local start = Engine.SystemMapGetOrbitPlannerStartTime()
-															 if start then
-																 return ui.Format.Duration(math.floor(start - now)), ""
-															 else
-																 return "now", ""
-															 end
-														 end,
-														 "Decrease time", "Reset time", "Increase time")
-									showDvLine(icons.orbit_reduce, icons.orbit_prograde, icons.orbit_increase, "prograde", ui.Format.Speed, "Thrust retrograde", "Reset prograde thrust", "Thrust prograde")
-									showDvLine(icons.orbit_reduce, icons.orbit_normal, icons.orbit_increase, "normal", ui.Format.Speed, "Thrust antinormal", "Reset normal thrust", "Thrust normal")
-									showDvLine(icons.orbit_reduce, icons.orbit_radial, icons.orbit_increase, "radial", ui.Format.Speed, "Thrust radially out", "Reset radial thrust", "Thrust radially in")
+			showDvLine(icons.orbit_reduce, icons.orbit_delta, icons.orbit_increase, "factor", function(i) return i, "x" end, "Decrease delta factor", "Reset delta factor", "Increase delta factor")
+			showDvLine(icons.orbit_reduce, icons.orbit_start_time, icons.orbit_increase, "starttime",
+			function(i)
+				local now = Game.time
+				local start = Engine.SystemMapGetOrbitPlannerStartTime()
+				if start then
+					return ui.Format.Duration(math.floor(start - now)), ""
+				else
+					return "now", ""
+				end
+			end,
+			"Decrease time", "Reset time", "Increase time")
+			showDvLine(icons.orbit_reduce, icons.orbit_prograde, icons.orbit_increase, "prograde", ui.Format.Speed, "Thrust retrograde", "Reset prograde thrust", "Thrust prograde")
+			showDvLine(icons.orbit_reduce, icons.orbit_normal, icons.orbit_increase, "normal", ui.Format.Speed, "Thrust antinormal", "Reset normal thrust", "Thrust normal")
+			showDvLine(icons.orbit_reduce, icons.orbit_radial, icons.orbit_increase, "radial", ui.Format.Speed, "Thrust radially out", "Reset radial thrust", "Thrust radially in")
 
-									ui.separator()
+			ui.separator()
 
-									local t = Engine.SystemMapGetOrbitPlannerTime()
-									ui.text(t and ui.Format.Datetime(t) or "now")
-									local r = false
-									r = timeButton(icons.time_backward_100x, "-10,000,000x",-10000000) or r
-									r = timeButton(icons.time_backward_10x, "-100,000x", -100000) or r
-									r = timeButton(icons.time_backward_1x, "-1,000x", -1000) or r
-									r = timeButton(icons.time_center, "Now", nil) or r
-									r = timeButton(icons.time_forward_1x, "1,000x", 1000) or r
-									r = timeButton(icons.time_forward_10x, "100,000x", 100000) or r
-									r = timeButton(icons.time_forward_100x, "10,000,000x", 10000000) or r
-									if not r then
-										if time_selected_button_icon == icons.time_center then
-											Engine.SystemMapAccelerateTime(nil)
-										else
-											Engine.SystemMapAccelerateTime(0.0)
-										end
-									end
-			end)
+			local t = Engine.SystemMapGetOrbitPlannerTime()
+			ui.text(t and ui.Format.Datetime(t) or "now")
+			local r = false
+			r = timeButton(icons.time_backward_100x, "-10,000,000x",-10000000) or r
+			r = timeButton(icons.time_backward_10x, "-100,000x", -100000) or r
+			r = timeButton(icons.time_backward_1x, "-1,000x", -1000) or r
+			r = timeButton(icons.time_center, "Now", nil) or r
+			r = timeButton(icons.time_forward_1x, "1,000x", 1000) or r
+			r = timeButton(icons.time_forward_10x, "100,000x", 100000) or r
+			r = timeButton(icons.time_forward_100x, "10,000,000x", 10000000) or r
+			if not r then
+				if time_selected_button_icon == icons.time_center then
+					Engine.SystemMapAccelerateTime(nil)
+				else
+					Engine.SystemMapAccelerateTime(0.0)
+				end
+			end
+		end)
 	end)
 end
 
@@ -331,9 +331,9 @@ local function displayOnScreenObjects()
 	local objects_grouped = Engine.SystemMapGetProjectedGrouped(collapse, 1e64)
 	if #objects_grouped == 0 then
 		ui.setNextWindowPos(Vector2(ui.screenWidth, ui.screenHeight) / 2 - ui.calcTextSize(lc.UNEXPLORED_SYSTEM_NO_SYSTEM_VIEW) / 2, "Always")
-	ui.withStyleColors({["WindowBg"] = svColor.SYSTEMNAME_BACK}, function()
-		ui.window("NoSystemView", {"NoTitleBar", "AlwaysAutoResize", "NoResize", "NoFocusOnAppearing", "NoBringToFrontOnFocus", "NoSavedSettings"},
-		function()
+		ui.withStyleColors({["WindowBg"] = svColor.SYSTEMNAME_BACK}, function()
+			ui.window("NoSystemView", {"NoTitleBar", "AlwaysAutoResize", "NoResize", "NoFocusOnAppearing", "NoBringToFrontOnFocus", "NoSavedSettings"},
+			function()
 				ui.text(lc.UNEXPLORED_SYSTEM_NO_SYSTEM_VIEW);
 			end)
 		end)
@@ -409,8 +409,8 @@ local function displayOnScreenObjects()
 				Engine.SystemMapCenterOn(mainObject.type, mainObject.base, mainObject.ref)
 			end
 		end
-		
-	objectCounter = objectCounter + 1
+
+		objectCounter = objectCounter + 1
 	end
 end
 
@@ -434,71 +434,61 @@ local function showTargetInfoWindow(obj)
 	ui.setNextWindowSize(Vector2(ui.screenWidth / 5, 0), "Always")
 	ui.setNextWindowPos(Vector2(20, (ui.screenHeight / 5) * 2 + 20), "Always")
 	ui.withStyleColors({["WindowBg"] = svColor.WINDOW_BACK}, function()
-			ui.window("TargetInfoWindow", {"NoTitleBar", "AlwaysAutoResize", "NoResize", "NoFocusOnAppearing", "NoBringToFrontOnFocus", "NoSavedSettings"},
-								function()
-									local data
-									-- system body
-									if obj.type == Projectable.OBJECT and obj.base == Projectable.SYSTEMBODY then
-										local systemBody = obj.ref
-										local name = systemBody.name
-										local rp = systemBody.rotationPeriod * 24 * 60 * 60
-										local r = systemBody.radius
-										local radius = nil
-										if r and r > 0 then
-											local v,u = ui.Format.Distance(r)
-											radius = v .. u
-										end
-										local sma = systemBody.semiMajorAxis
-										local semimajoraxis = nil
-										if sma and sma > 0 then
-											local v,u = ui.Format.Distance(sma)
-											semimajoraxis = v .. u
-										end
-										local op = systemBody.orbitPeriod * 24 * 60 * 60
-										data = {
-											{ name = lc.NAME_OBJECT,
-												value = name },
-											{ name = lc.DAY_LENGTH .. lc.ROTATIONAL_PERIOD,
-												value = rp > 0 and ui.Format.Duration(rp, 2) or nil },
-											{ name = lc.RADIUS,
-												value = radius },
-											{ name = lc.SEMI_MAJOR_AXIS,
-												value = semimajoraxis },
-											{ name = lc.ORBITAL_PERIOD,
-												value = op and op > 0 and ui.Format.Duration(op, 2) or nil }
-										}
-									-- physical body
-									elseif obj.type == Projectable.OBJECT and obj.ref:IsShip() then
-										local body = obj.ref
-										local name = body.label
-										data = {{ name = lc.NAME_OBJECT,
-															value = name },
-										}
-										-- TODO: the advanced target scanner should add additional data here,
-										-- but we really do not want to hardcode that here. there should be
-										-- some kind of hook that the target scanner can hook into to display
-										-- more info here.
-										-- This is what should be inserted:
-										table.insert(data, { name = "Ship Type",
-																				 value = body:GetShipType()
-										})
-										if player:GetEquipCountOccupied('target_scanner') > 0 or player:GetEquipCountOccupied('advanced_target_scanner') > 0 then
-										local hd = body:GetEquip("engine", 1)
-										table.insert(data, { name = "Hyperdrive",
-																				 value = hd and hd:GetName() or lc.NO_HYPERDRIVE
-										})
-										table.insert(data, { name = lc.MASS,
-																				 value = Format.MassTonnes(body:GetStats().staticMass)
-										})
-										table.insert(data, { name = lc.CARGO,
-																				 value = Format.MassTonnes(body:GetStats().usedCargo)
-										})
-									end
-									else
-										data = {}
-									end
-									tabular(data)
-			end)
+		ui.window("TargetInfoWindow", {"NoTitleBar", "AlwaysAutoResize", "NoResize", "NoFocusOnAppearing", "NoBringToFrontOnFocus", "NoSavedSettings"},
+		function()
+			local data
+			-- system body
+			if obj.type == Projectable.OBJECT and obj.base == Projectable.SYSTEMBODY then
+				local systemBody = obj.ref
+				local name = systemBody.name
+				local rp = systemBody.rotationPeriod * 24 * 60 * 60
+				local r = systemBody.radius
+				local radius = nil
+				if r and r > 0 then
+					local v,u = ui.Format.Distance(r)
+					radius = v .. u
+				end
+				local sma = systemBody.semiMajorAxis
+				local semimajoraxis = nil
+				if sma and sma > 0 then
+					local v,u = ui.Format.Distance(sma)
+					semimajoraxis = v .. u
+				end
+				local op = systemBody.orbitPeriod * 24 * 60 * 60
+				data = {
+					{ name = lc.NAME_OBJECT,
+					value = name },
+					{ name = lc.DAY_LENGTH .. lc.ROTATIONAL_PERIOD,
+					value = rp > 0 and ui.Format.Duration(rp, 2) or nil },
+					{ name = lc.RADIUS,
+					value = radius },
+					{ name = lc.SEMI_MAJOR_AXIS,
+					value = semimajoraxis },
+					{ name = lc.ORBITAL_PERIOD,
+					value = op and op > 0 and ui.Format.Duration(op, 2) or nil }
+				}
+				-- physical body
+			elseif obj.type == Projectable.OBJECT and obj.ref:IsShip() then
+				local body = obj.ref
+				local name = body.label
+				data = {{ name = lc.NAME_OBJECT, value = name }, }
+				-- TODO: the advanced target scanner should add additional data here,
+				-- but we really do not want to hardcode that here. there should be
+				-- some kind of hook that the target scanner can hook into to display
+				-- more info here.
+				-- This is what should be inserted:
+				table.insert(data, { name = "Ship Type", value = body:GetShipType() })
+				if player:GetEquipCountOccupied('target_scanner') > 0 or player:GetEquipCountOccupied('advanced_target_scanner') > 0 then
+					local hd = body:GetEquip("engine", 1)
+					table.insert(data, { name = "Hyperdrive", value = hd and hd:GetName() or lc.NO_HYPERDRIVE })
+					table.insert(data, { name = lc.MASS, value = Format.MassTonnes(body:GetStats().staticMass) })
+					table.insert(data, { name = lc.CARGO, value = Format.MassTonnes(body:GetStats().usedCargo) })
+				end
+			else
+				data = {}
+			end
+			tabular(data)
+		end)
 	end)
 end
 
