2d39a7d40e9a2de084e058053de6b0000d090dc9
Gliese852 <gliese852@gmail.com>
2025-10-31 21:58:12 +0300

    Set default fuel reserve in GetDurationForDistance

--- src/lua/LuaShip.cpp
+++ src/lua/LuaShip.cpp
@@ -620,6 +620,10 @@ static int l_ship_get_duration_for_distance(lua_State *l)
 	const ShipType *st = ship->GetShipType();
 	const shipstats_t ss = ship->GetStats();
 	double fuel_reserve = ship->GetPropulsion()->GetFuelReserve();
+
+	// for planning a flight without the autopilot turned on
+	if (!ship->AIIsActive()) fuel_reserve = 0.25;
+
 	PrecalcPath pp(
 		distance, // distance
 		start_vel,	  // velocity at start
