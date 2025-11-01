06769c7c5917c081a34d629ac0efc1534628634c
Gliese852 <gliese852@gmail.com>
2025-11-01 11:53:06 +0300

    Don't reset BBS table scroll

--- data/pigui/modules/station-view/02-bulletinBoard.lua
+++ data/pigui/modules/station-view/02-bulletinBoard.lua
@@ -254,7 +254,6 @@ StationView:registerView({
 	draw = renderBulletinBoard,
 	refresh = function ()
 		refresh()
-		bulletinBoard.scrollReset = true
 	end,
 	debugReload = function()
 		package.reimport()
