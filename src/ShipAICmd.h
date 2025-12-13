31eb97dfb732acfe1ba567af1e33f374a6d4b7c6
Gliese852 <gliese852@gmail.com>
2025-12-13 23:10:24 +0300

    T: cleanup gettarget methods in shipai

--- src/ShipAICmd.h
+++ src/ShipAICmd.h
@@ -62,7 +62,7 @@ public:
 
 	CmdName GetType() const { return m_cmdName; }
 
-	virtual const Body *GetTarget() const { return nullptr; }
+	virtual Body *GetTarget() const { return nullptr; }
 
 protected:
 	DynamicBody *m_dBody;
@@ -86,7 +86,7 @@ public:
 	virtual void PostLoadFixup(Space *space);
 
 	virtual void OnDeleted(const Body *body);
-	virtual const Body *GetTarget() const { return (Body*)m_target; }
+	virtual Body *GetTarget() const { return (Body*)m_target; }
 
 private:
 	enum EDockingStates {
@@ -131,6 +131,8 @@ public:
 
 	virtual void OnDeleted(const Body *body);
 
+	virtual Body *GetTarget() const { return m_target; }
+
 private:
 	Body *m_target;		   // target for vicinity. Either this or targframe is 0
 	double m_dist;		   // vicinity distance
@@ -167,6 +169,8 @@ public:
 		m_targmode = 0;
 	}
 
+	virtual Body *GetTarget() const { return m_obstructor; }
+
 protected:
 	void Setup(Body *obstructor, double alt, double vel, int mode);
 	double MaxVel(double targdist, double targalt);
@@ -193,7 +197,7 @@ public:
 
 	virtual void OnDeleted(const Body *body);
 
-	virtual const Body* GetTarget() const { return (Body*)m_target; }
+	virtual Body* GetTarget() const { return (Body*)m_target; }
 
 private:
 	struct GunStats {
@@ -221,7 +225,7 @@ public:
 
 	virtual void OnDeleted(const Body *body);
 
-	const Body* GetTarget() const { return m_target; }
+	virtual Body* GetTarget() const { return m_target; }
 private:
 	Body *m_target;
 	int m_targetIndex; // used during deserialisation
@@ -245,6 +249,8 @@ public:
 	virtual void PostLoadFixup(Space *space);
 
 	virtual void OnDeleted(const Body *body);
+
+	virtual Body* GetTarget() const { return m_target; }
 
 private:
 	DynamicBody *m_target; // target frame for waypoint
