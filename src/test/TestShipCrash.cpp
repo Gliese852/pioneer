124874f8abecf18d20881d78b066aa0df6ea7e73
Gliese852 <gliese852@gmail.com>
2026-01-23 23:10:19 +0300

    TEST: running pioneer!

--- src/test/TestShipCrash.cpp
+++ src/test/TestShipCrash.cpp
@@ -1,14 +1,40 @@
 #include "Pi.h"
 #include "lua/Lua.h"
 #include "pigui/LuaPiGui.h"
+#include "core/GuiApplication.h"
 
 #include "doctest.h"
 
+class TestLoop : public Application::Lifecycle {
+	void Start() override
+	{
+		m_counter = 0;
+		std::cout << "STARTING TESTLOOP";
+	}
+	void Update(float deltaTime) override
+	{
+		++m_counter;
+		std::cout << "COUNTER:" << m_counter;
+		if (m_counter > 500) {
+			RequestEndLifecycle();
+		}
+	}
+	void End() override
+	{
+		std::cout << "END TESTLOOP";
+	}
+
+	int m_counter;
+};
+
 TEST_CASE("ship_crash")
 {
+	RefCountedPtr<Application::Lifecycle> testLoop;
+	testLoop.Reset(new TestLoop());
 	std::map<std::string, std::string> options;
-	Pi::Init(options, false);
-	Lua::Init(Pi::GetAsyncJobQueue());
-	PiGui::Lua::Init();
-	Lua::InitModules();
+
+	Pi::Init(options, true);
+	std::cout << "QUEUE TESTLOOP\n";
+	Pi::GetApp()->QueueLifecycle(testLoop);
+	Pi::GetApp()->Run();
 }
