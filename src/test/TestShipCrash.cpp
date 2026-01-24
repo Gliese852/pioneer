147963a6b6ac7fd15a22307ebd79b593731f9815
Gliese852 <gliese852@gmail.com>
2026-01-24 22:24:27 +0300

    T: load savegame

--- src/test/TestShipCrash.cpp
+++ src/test/TestShipCrash.cpp
@@ -2,6 +2,9 @@
 #include "lua/Lua.h"
 #include "pigui/LuaPiGui.h"
 #include "core/GuiApplication.h"
+#include "SaveGameManager.h"
+#include "Json.h"
+#include "Game.h"
 
 #include "doctest.h"
 
@@ -9,19 +12,24 @@ class TestLoop : public Application::Lifecycle {
 	void Start() override
 	{
 		m_counter = 0;
-		std::cout << "STARTING TESTLOOP";
+		std::cout << "STARTING TESTLOOP" << std::endl;
+		Json rootNode = SaveGameManager::LoadGameToJson("test_crash");
+		CHECK(rootNode.is_object());
+
+		Pi::game = new Game(rootNode);
+
 	}
 	void Update(float deltaTime) override
 	{
 		++m_counter;
-		std::cout << "COUNTER:" << m_counter;
+		std::cout << "COUNTER:" << m_counter << std::endl;
 		if (m_counter > 500) {
 			RequestEndLifecycle();
 		}
 	}
 	void End() override
 	{
-		std::cout << "END TESTLOOP";
+		std::cout << "END TESTLOOP" << std::endl;
 	}
 
 	int m_counter;
@@ -34,7 +42,9 @@ TEST_CASE("ship_crash")
 	std::map<std::string, std::string> options;
 
 	Pi::Init(options, true);
+
 	std::cout << "QUEUE TESTLOOP\n";
 	Pi::GetApp()->QueueLifecycle(testLoop);
+
 	Pi::GetApp()->Run();
 }
