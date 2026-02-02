51c12676886c73c13c9f5a2a77298ad49203e686
Gliese852 <gliese852@gmail.com>
2026-02-02 09:44:29 +0300

    T: starting test game and putting ship to 0

--- src/test/TestShipThrust.cpp
+++ src/test/TestShipThrust.cpp
@@ -2,21 +2,91 @@
 #include "Space.h"
 #include "FileSystem.h"
 #include "lua/Lua.h"
+#include "lua/LuaEvent.h"
+#include "pigui/LuaPiGui.h"
 #include "Pi.h"
+#include "BaseSphere.h"
+#include "graphics/Graphics.h"
+#include "graphics/dummy/RendererDummy.h"
+#include "core/OS.h"
+#include "SpaceStationType.h"
+#include "ModelCache.h"
+#include "NavLights.h"
+#include "Shields.h"
+#include "Body.h"
+#include "Ship.h"
+#include "Frame.h"
 
 #include "doctest.h"
 
+static std::basic_ostream<char, std::char_traits<char>> &operator<<(std::basic_ostream<char, std::char_traits<char>> &log, const vector3d &v)
+{
+	log << v.x << " " << v.y << " " << v.z;
+	return log;
+}
+
+static std::basic_ostream<char, std::char_traits<char>> &operator<<(std::basic_ostream<char, std::char_traits<char>> &log, const matrix3x3d &m)
+{
+	log << m.VectorX() << std::endl << m.VectorY() << std::endl << m.VectorZ();
+	return log;
+}
+
+
 TEST_CASE("ship_thrust")
 {
-	try {
-		std::map<std::string, std::string> options;
-		Pi::Init(options, true);
-		Lua::Init(Pi::GetAsyncJobQueue());
-		FileSystem::Init();
-		SystemPath path{ 0, 0, 0, 1, 2 };
-		Game g(path, 0);
-	} catch (...) {
-		Game *pg = nullptr;
-		pg->IsNormalSpace();
+	std::map<std::string, std::string> options;
+	Pi::Init(options, true);
+	Lua::Init(Pi::GetAsyncJobQueue());
+	PiGui::Lua::Init();
+	LuaEvent::Init();
+	FileSystem::Init();
+	BaseSphere::Init(Pi::renderer);
+	Pi::modelCache = new ModelCache(Pi::renderer);
+	Shields::Init(Pi::renderer);
+	NavLights::Init(Pi::renderer);
+	SpaceStationType::Init();
+	assert(Pi::modelCache);
+	SystemPath path{ -2, -4, -1, 0, 1 };
+	Game g(path, 0);
+
+	Ship *s = nullptr;
+
+	std::cout << "BODIES: " << g.GetSpace()->GetNumBodies() << std::endl;
+	auto bodies = g.GetSpace()->GetBodies();
+	for(auto it = bodies.begin(); it != bodies.end(); ++it) {
+		std::cout
+			<< "body: " << (*it)->GetLabel()
+			<< " pos: " << (*it)->GetPosition()
+			<< std::endl;
+
+		if ((*it)->IsType(ObjectType::SHIP)) {
+			s = static_cast<Ship*>(*it);
+		}
 	}
+
+	assert(s);
+
+	s->SetFrame(0);
+	s->SetPosition({ 0, 0, 0 });
+	s->SetVelocity({ 0, 0, 0 });
+
+	std::cout << std::endl << "THE SHIP";
+	auto f = Frame::GetFrame(s->GetFrame());
+	auto fb = f->GetBody();
+	auto fsb = f->GetSystemBody();
+
+	if (fb) {
+		std::cout << " fb: " << fb->GetLabel();
+	} else {
+		std::cout << " fb: NO";
+	}
+
+	if (fsb) {
+		std::cout << " fsb: " << fsb->GetName();
+	}
+
+	std::cout << " pos: " << s->GetPosition() << " vel: " << s->GetVelocity()
+		<< std::endl << " ori: " << std::endl << s->GetOrient();
+
+	std::cout << std::endl;
 }
