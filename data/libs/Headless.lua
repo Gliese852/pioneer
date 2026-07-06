b6a2ff4ed092235b02ef9021cde774945aed0fa9
Gliese852 <gliese852@gmail.com>
2026-07-06 22:22:57 +0300

    headless: improve status, add savegame

--- data/libs/Headless.lua
+++ data/libs/Headless.lua
@@ -1,4 +1,5 @@
 local Engine = require 'Engine'
+local ui = require 'pigui'
 
 local Headless = {}
 
@@ -18,6 +19,11 @@ function Headless.LoadGame(filename)
 	Game.SetRealtimeMode(true)
 end
 
+function Headless.SaveGame(filename)
+	local Game = require 'Game'
+	Game.SaveGame(filename)
+end
+
 function Headless.EndGame()
 	local Game = require 'Game'
 	Game.EndGame()
@@ -35,6 +41,46 @@ function Headless.Profile()
 	return Engine.RequestProfileFrame()
 end
 
+local function estimate(target)
+	local Game = require 'Game'
+	local player = Game.player
+	local velocity = player:GetVelocityRelTo(target)
+	local position = player:GetPositionRelTo(target)
+	local approach_speed = position:dot(velocity) / position:length()
+	local altitude = player:GetAltitudeRelTo(target)
+	local estimate = player:GetDurationForDistance(altitude, -approach_speed, 0.9)
+	return estimate
+end
+
+function Headless.FlightData()
+	local Game = require 'Game'
+	local player = Game.player
+	local fstate = player.flightState
+	local s = ""
+	if fstate ~= 'FLYING' then
+		return "STATUS: " .. fstate
+	else
+		s = "STATUS: FLYING"
+	end
+
+	local s = ""
+
+	aTarget = Game.player:GetAutopilotTarget()
+	if not aTarget then
+		s = s .. ", NO AI TARGET";
+		return s;
+	end
+
+	local altitude = player:GetAltitudeRelTo(aTarget)
+	local altitude_txt, altitude_unit = ui.Format.DistanceUnit(altitude)
+
+	local estimate_txt = ui.Format.Duration(estimate(aTarget))
+	s = s .. ", AI TARGET: " .. tostring(aTarget.label) .. "\nEST: " .. estimate_txt .. " DST: " .. altitude_txt .. " " .. altitude_unit
+
+
+	return s
+end
+
 function Headless.Status()
 	local Game = require 'Game'
 
@@ -43,6 +89,7 @@ function Headless.Status()
 
 	local s = "TIME: " .. time .. " PERF: " .. Headless.Perf()
 	s = s .. "\nAI: " .. Game.player:GetAIStatusText()
+	s = s .. "\n" .. Headless.FlightData()
 
 	return s
 end
