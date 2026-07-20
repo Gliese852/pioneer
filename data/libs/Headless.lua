f8db8f6a1cbcde35b6c7b13fa51dc47ba0db524d
Gliese852 <gliese852@gmail.com>
2026-07-20 12:24:00 +0300

    headless: hyperspace tools

--- data/libs/Headless.lua
+++ data/libs/Headless.lua
@@ -1,5 +1,6 @@
 local Engine = require 'Engine'
 local ui = require 'pigui'
+local utils = require 'utils'
 
 local Headless = {}
 
@@ -63,13 +64,6 @@ local ftime = ui.Format.Duration
 function Headless.FlightData()
 	local Game = require 'Game'
 	local player = Game.player
-	local fstate = player.flightState
-	local s = ""
-	if fstate ~= 'FLYING' then
-		return "STATUS: " .. fstate
-	else
-		s = "STATUS: FLYING"
-	end
 
 	local s = ""
 
@@ -118,6 +112,96 @@ function Headless.FlightData()
 	return s
 end
 
+function Headless.HyperStatus()
+	local Game = require 'Game'
+	local path,destName = Game.player:GetHyperspaceDestination()
+	local percent = Game.GetHyperspaceTravelledPercentage() * 100
+	local due = Game.GetHyperspaceEndTime()
+
+	if not path or not destName or not percent or not due then
+		return 'NO DATA'
+	end
+
+	local s = "DEST: " .. destName .. " (" .. path.sectorX .. " " .. path.sectorY .. " " .. path.sectorZ .. ")"
+  	local s = s .. " " .. string.format("%2.1f", percent) .. "%"
+	local s = s .. "\nARRIVAL: " .. ui.Format.Date(due)
+	local s = s .. " ETA: " ..  ui.Format.Duration(due - Game.time, 3)
+
+	return s
+end
+
+function Headless.HyperRoute(n)
+	local Game = require 'Game'
+	local sectorView = Game.sectorView
+	local route = sectorView:GetRoute()
+	local start = Game.system and Game.system.path
+	if not start then
+		start = Game.InHyperspace() and Game.GetHyperspaceSource() or sectorView:GetCurrentSystemPath()
+	end
+
+	local player = Game.player
+
+	local skippedDist = 0
+	local skippedNum = 0
+	local s = "SOURCE: " .. ui.Format.SystemPath(start) .. "\n"
+	for i, path in pairs(route) do
+		local status, distance, fuel, duration = player:GetHyperspaceDetails(start, path)
+		if not n or n == #route or i == #route or i < n - 1 then
+			s = s .. i .. ": " .. ui.Format.SystemPath(path) .. " " .. ui.Format.Number(distance, 2) .. " ly\n"
+		else
+			skippedDist = skippedDist + distance
+			skippedNum = skippedNum + 1
+			if i == #route - 1 then
+				s = s .. "..." .. skippedNum .. " jumps " .. ui.Format.Number(skippedDist, 2) .. " ly\n"
+			end
+		end
+		start = path
+	end
+
+	return s
+end
+
+function Headless.Jump(op)
+
+	local Game = require 'Game'
+	local player = Game.player
+
+	local s = ""
+	local allowJump = 2 -- 0 cant 1 legal 2 illegal
+
+	local targetpath = player:GetHyperspaceTarget()
+	s = s .. "\nTARGET: " .. ui.Format.SystemPath(targetpath)
+	s = s .. "\nCAN JUMP: "
+
+	if player:CanHyperjumpTo(targetpath) then
+		if player:IsDocked() or player:IsLanded() then
+			s = s .. "NO, LANDED/DOCKED"
+		elseif player:IsHyperspaceActive() then
+			s = s .. "NO, COUNTING"
+		elseif not player:IsHyperjumpAllowed() then
+			s = s .. "BETTER NO, NOT ALLOWED"
+			allowJump = 1
+		end
+		s = s .. "YES, OK"
+		allowJump = 0
+	else
+		s = s .. "NO, UNKNOWN REASON"
+	end
+
+	if op and op <= 2 and op > allowJump then
+		s = s .. "\ninitiating jump"
+		player:HyperjumpTo(player:GetHyperspaceTarget())
+	else
+		if not op then
+			s = s .. "\nuse arg 1 to jump legal, arg 2 to to jump even illegal"
+		else
+			s = s .. "\ncan't jump with op " .. op
+		end
+	end
+
+	return s
+end
+
 function Headless.Status()
 	local Game = require 'Game'
 
@@ -126,7 +210,16 @@ function Headless.Status()
 
 	local s = "TIME: " .. time .. " PERF: " .. Headless.Perf()
 	s = s .. "\nAI: " .. Game.player:GetAIStatusText()
-	s = s .. "\n" .. Headless.FlightData()
+
+	local fstate = Game.player.flightState
+    s = s .. "\nSTATUS: " .. fstate;
+
+	if fstate == 'FLYING' then
+		s = s .. " " .. Headless.FlightData()
+	elseif fstate == 'HYPERSPACE' then
+		s = s .. " " .. Headless.HyperStatus()
+		s = s .. "\n" ..Headless.HyperRoute(5)
+	end
 
 	return s
 end
